7f29f5d7cde49195af29a6d05f97aded11e1f8c0
Brandon Franks <captainxcuddles@hotmail.com>
2022-06-12 12:59:11 -0600

    adding questions

--- assets/questions.js
+++ assets/questions.js
@@ -1,8 +1,40 @@
 var questions = [
     {
-        question: "Where is the dog?",
-        choices: ["a", "b", "c", "d",],
-        answer: "a",
-        
-    }
-]
+      question: "What does CSS do?",
+      answers: [
+        { text: "Nothing", correct: false },
+        { text: "Adds style to HTML", correct: true },
+        { text: "It is the bones of the web page", correct: false },
+        { text: "Adds function to the web page", correct: false },
+      ],
+    },
+    {
+      question: "How do you write and array in JavaScript?",
+      answers: [
+        { text: "var letters = ('a','b','c')", correct: true },
+        { text: "var letters = 'a','b','c'", correct: false },
+        { text: "var letters = (a,b,c)", correct: false },
+        { text: "var letters = a,b,c", correct: false },
+      ],
+    },
+    {
+      question: "What is a way to write a function in Javascript?",
+      answers: [
+        { text: "function;nameOfFunction()", correct: false },
+        { text: "function nameOfFunction()", correct: true },
+        { text: "function:nameOfFunction()", correct: false },
+        { text: "function+nameOfFunction()", correct: false },
+      ],
+    },
+    {
+      question: "How do you put Javascript into HTML?",
+      answers: [
+        //prettier-ignore
+        { text: "<javascript>", correct: false },
+        { text: "<script>", correct: true },
+        { text: "<java>", correct: false },
+        { text: "<js>", correct: false },
+      ],
+    },
+  ];
+  
